90d471a1efe8b44405451101db4fa49183bd0009
Phil Kulak <pkulak@gmail.com>
2022-06-15 15:28:28 -0700

    Removed some stuff I don't use anymore.

--- install/custom-setup.sh
+++ install/custom-setup.sh
@@ -5,14 +5,14 @@ set -o allexport; source /root/tmp/install/config; set +o allexport
 pacman --noconfirm -Sy archlinux-keyring
 
 # Install all my goodies
-su $USER_NAME -c "yay --noconfirm -S sway swayidle alacritty cage \
+su $USER_NAME -c "yay --noconfirm -S sway swayidle alacritty \
   waybar xorg-server-xwayland wl-clipboard fish pipewire pipewire-pulse \
   xdg-desktop-portal-wlr otf-font-awesome noto-fonts noto-fonts-cjk \
   noto-fonts-emoji noto-fonts-extra playerctl pavucontrol mesa \
-  rsync zathura zathura-pdf-mupdf imv mpv youtube-dl firefox libnotify \
+  rsync zathura zathura-pdf-mupdf imv mpv firefox libnotify \
   ttf-ubuntu-font-family element-desktop qt5-wayland mako grim slurp \
   papirus-icon-theme python-cssselect python-requests python-lxml python-pip \
-  fuse fzf sshfs gocryptfs htop wf-recorder mlocate ulauncher spotify \
+  fuse htop wf-recorder mlocate ulauncher \
   redshift-wayland-git ant-dracula-gtk-theme lf dragon-drag-and-drop \
   neovim-symlinks autotiling-git"
 
